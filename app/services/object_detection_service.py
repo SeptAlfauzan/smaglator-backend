1b541cceca3d8d699c382ee606ec37c38f0737cb
SeptAlfauzan <alfauzansepta@gmail.com>
2023-08-24 21:56:20 +0700

    change from joblib to pickle, to load ml model

--- app/services/object_detection_service.py
+++ app/services/object_detection_service.py
@@ -1,3 +1,4 @@
+import pickle
 from typing import List
 import joblib
 import pandas as pd
@@ -8,7 +9,9 @@ from app.model.hand_gesture_feature import HandGestureFeaturesModel
 
 
 def predict_handgesture_language(input: List[HandGestureFeaturesModel]) -> List[str]:
-    model: RandomForestClassifier = joblib.load("./app/assets/ml_model/datasetsibi.pkl")
+    model: RandomForestClassifier = pickle.load(
+        open("./app/assets/ml_model/datasetsibi.pkl", "rb")
+    )
 
     data_dict = [data.dict() for data in input]
     input_x = pd.DataFrame.from_dict(data_dict)
