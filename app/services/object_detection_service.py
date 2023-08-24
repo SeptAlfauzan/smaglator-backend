441d30726d500c94c6cf48618915684147de422c
SeptAlfauzan <alfauzansepta@gmail.com>
2023-08-24 20:58:07 +0700

    test adding scikit-learn library in vercel

--- app/services/object_detection_service.py
+++ app/services/object_detection_service.py
@@ -2,14 +2,13 @@ from typing import List
 import joblib
 import pandas as pd
 import numpy as np
-
-# from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 from app.model.hand_gesture_feature import HandGestureFeaturesModel
 
 
 def predict_handgesture_language(input: List[HandGestureFeaturesModel]) -> List[str]:
-    model = joblib.load("./app/assets/ml_model/datasetsibi.pkl")
+    model: RandomForestClassifier = joblib.load("./app/assets/ml_model/datasetsibi.pkl")
 
     data_dict = [data.dict() for data in input]
     input_x = pd.DataFrame.from_dict(data_dict)
